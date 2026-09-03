304787c4014b9c638f56e6b9430fc2d26fe3ee5a
Dewashish Solanke <dewashish.solanke27@gmail.com>
2026-09-03 09:46:10 +0530

    modified the code

--- api/main.py
+++ api/main.py
@@ -11,7 +11,10 @@ app = FastAPI(title="ThoughtKeeper API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=[
+        "https://thoughtkeeper-ebon.vercel.app",
+        "https://thoughtkeeper-h1ojrdsf-thought-keeper.vercel.app",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
